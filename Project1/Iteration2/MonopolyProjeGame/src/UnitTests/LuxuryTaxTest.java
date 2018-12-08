bf2c08a36a6930336660278544bab7c0ad2b7ea8
Emrullah Başoda <basodaemrullah@gmail.com>
2018-12-08 20:21:07 +0300

    Add files via upload

---
+++ Project1/Iteration2/MonopolyProjeGame/src/UnitTests/LuxuryTaxTest.java
@@ -0,0 +1,20 @@
+package Monopoly.UnitTests;
+
+import Monopoly.Player.Money;
+import Monopoly.Player.Player;
+
+import org.junit.Assert;
+
+
+
+public class LuxuryTaxTest {
+    public Player a=new Player();
+    private Money tax=new Money(15000);
+
+    @org.junit.Test
+    public void doAction() {
+        double PlayerMoney=a.getMoney().getMoney();
+        double Tax=tax.getMoney();
+        Assert.assertEquals(PlayerMoney-Tax,a.getMoney().getMoney()-tax.getMoney(),1);
+    }
+}
